643ada3df2b2a5ef052747ddbf0df9e93914ac33
Sekki16 <kenrogerdomingo@gmail.com>
2023-12-02 19:23:46 +0800

    modify

--- projectsite/cardquest/admin.py
+++ projectsite/cardquest/admin.py
@@ -9,5 +9,5 @@ class TrainerAdmin(admin.ModelAdmin):
 
 @admin.register(PokemonCard)
 class PokemonCardAdmin(admin.ModelAdmin):
-    list_display = ("name", "rarity", "hp", "card_type", "attack", "description", "weakness", "card_number", "release_date", "evolution_stage", "abilities")
-    search_fields = ("name", "rarity", "card_type",)
+    list_display = ("name", "rarity")
+    search_fields = ("name",)
